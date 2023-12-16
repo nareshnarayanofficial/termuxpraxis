5d72c377f24cb1cc01367dff0755925f3d8fda25
Naresh Narayan <88131733+nareshnarayanofficial@users.noreply.github.com>
2023-12-16 09:21:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simulate loading
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        document.querySelector('.loading').style.display = 'none';
+    }, 2000); // Simulating a 2-second loading time
+});
